2a313dbab1ff971dbc2d9bec4c35a50adfd93d9a
Tygo van Dorst <D295661@edu.curio.nl>
2023-09-15 12:39:14 +0200

    afs

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    public function getPriceAttribute($value)
+    {
+        $discount = $value * ($this->discount / 100);
+        $final_price = $value - $discount;
+        return number_format($final_price, 2);
+    }
     public function types()
     {
     	return $this->hasMany(Type::class);
     }
+    
 }
--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -19,6 +19,7 @@
 			<th>Speltak</th>
 			<th>Bedrag</th>
 			<th>Betaling</th>
+			<th>Levering</th>
 		</tr>
 		@foreach($orders as $order)
 			<tr>
@@ -30,6 +31,9 @@
 				<td>&euro;{{ number_format($order->amount, 2) }}</td>
 				<td>
 					{!! $order->payed ? '<span class="badge badge-success">betaald</span>' : '<span class="badge badge-warning">niet betaald</span>' !!}
+				</td>
+				<td>
+					{!! $order->delivered ? '<span class="badge badge-success">bezorgd</span>' : '<span class="badge badge-warning">niet bezorgd</span>' !!}
 				</td>
 			</tr>
 		@endforeach
